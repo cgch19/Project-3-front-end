1cb0cf92a1a5c074c304f2f71ba6e66159236d29
blisssr77 <robinchae55@gmail.com>
2024-03-30 16:36:40 -0700

    making create artist page

---
+++ front-end/src/pages/Index.js
@@ -0,0 +1,14 @@
+import { Content, Button, Heading } from "react-bulma-components";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ArtistContext } from "../App";
+
+// const Index = (props) => {
+//     const artists = useContext(ArtistContext)
+
+//     const loaded = () => {
+//         return artists.map((artist, idx) => {
+//             return ()
+//         })
+//     }
+// }
