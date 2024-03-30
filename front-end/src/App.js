797a659de364189fdcd40194a04e4eca76cbf759
Sinai161 <ashleygalvan161@yahoo.com>
2024-03-30 12:50:55 -0700

    sign up and login working

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Nav from './components/Nav';
 import Signup from './components/Signup';
 import Homepage from './pages/Homepage';
+import Profile from './pages/Profile';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate()
+  const URL = "http://localhost:4000/api/"
 
   const handleLogin = async(user) => {
     const response = await fetch(URL + "auth/login", {
@@ -15,14 +20,75 @@ function App() {
       },
       body: JSON.stringify(user)
     })
+    const data = await response.json()
+    if(response.status !== 200) {
+      return data
+    }
+    localStorage.setItem("authToken", data.token)
+    setIsLoggedIn(true)
+
+    navigate(`/profile/${data.id}`)
   }
+
+  const handleSignUp = async(user) => {
+    const response = await fetch(URL + "auth/signup", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user)
+    })
+    const data = await response.json()
+    console.log(data)
+    navigate("/login")
+  }
+
+  const handleLogout = () => {
+    console.log(" in handle log")
+    localStorage.removeItem("authToken")
+    setIsLoggedIn(false)
+    navigate("/")
+  }
+
+  const [user, setUser] = useState(null)
+
+  const fetchUser = async(id) => {
+    const token = localStorage.getItem("authToken")
+    if(token) {
+      const response = await fetch(URL + `user/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "authorization": token
+        }
+      })
+      const data = await response.json()
+      setUser(data.data)
+    }else {
+      console.log("no token")
+    }
+  }
+
+  useEffect(() => {
+    let token = localStorage.getItem("authToken")
+
+    if(!token) {
+      setIsLoggedIn(false)
+    }else {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav isLoggedIn={isLoggedIn} handleLogout={handleLogout}/>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login handleLogin={handleLogin} />} />
+        <Route path="/signup" element={<Signup
+        handleSignUp={handleSignUp} />} />
+        <Route path="/profile/:id" element={<Profile fetchUser={fetchUser} user={user}/>}/>
       </Routes>
     </div>
   );
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -1,19 +1,45 @@
 import { Link } from "react-router-dom"
-import React from "react"
 
-const Nav = () => {
-    console.log("Nav component is rendered");
-  return (
-    <nav className="navbar">
+const Nav = (props) => {
+  
+  const loggedInLink = (
     <div>
-      <Link to="/" className="nav-link">Home</Link>
+      <button onClick={props.handleLogout}>Logout</button>
     </div>
-    <div>
-      <Link to="/login" className="nav-link">Login</Link>
-      <Link to="/signup" className="nav-link">Sign Up</Link>
-    </div>
-  </nav>
   )
-} 
+
+  const noAuthLinks = (
+    <div>
+    <Link to="/login" className="nav-link">Login</Link>
+    <Link to="/signup" className="nav-link">Sign Up</Link>
+    </div>
+  )
+
+  return (
+    <nav>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        {props.isLoggedIn ? loggedInLink : noAuthLinks}
+      </div>
+    </nav>
+  )
+}
 
 export default Nav
+
+// const Nav = () => {
+//     console.log("Nav component is rendered");
+//   return (
+//     <nav className="navbar">
+//     <div>
+//       <Link to="/" className="nav-link">Home</Link>
+//     </div>
+//     <div>
+//       <Link to="/login" className="nav-link">Login</Link>
+//       <Link to="/signup" className="nav-link">Sign Up</Link>
+//     </div>
+//   </nav>
+//   )
+// } 
