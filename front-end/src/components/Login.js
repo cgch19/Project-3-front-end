6e1bb02e25e66b96bd989e8e7117a76616397cc4
cgch8 <carloschie15@gmail.com>
2024-03-30 13:57:06 -0700

    s

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -19,10 +19,6 @@ return (
     <div className="form-container">
       <h2>Log In</h2>
     <form onSubmit={handleSubmit}>
-    <span>
-        <label htmlFor="username">Username: </label>
-        <input type="text" name="username" onChange={handleChange}/>
-    </span>
     <span>                
         <label htmlFor="email">Email: </label>
         <input type="email" name="email" onChange={handleChange}/>
