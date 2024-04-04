81f232d047b067870bd89a51ea932d8e4dfb14e6
blisssr77 <robinchae55@gmail.com>
2024-04-03 18:22:44 -0700

    last work

--- front-end/src/pages/Index.js
+++ front-end/src/pages/Index.js
@@ -3,26 +3,9 @@ import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { ArtistContext } from "../App";
 
-
-
-
-
 const Index = (props) => {
     const artists = useContext(ArtistContext)
     console.log(artists)
-
-
-//     const loaded = () => {
-//         return artists.map((artist, idx) => {
-//             return (
-//                 <Content key={idx}>
-//                     <Heading>{artist.artist}</Heading>
-//                     <p>{artist.song}</p>
-//             )
-//         })
-//     }
-//
-//
 
     const loaded = () => {
         return artists.map((artist, idx) => {
