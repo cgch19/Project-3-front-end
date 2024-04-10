35927dfdc95dd8d2516e62075b180c337992a3a8
Sinai161 <ashleygalvan161@yahoo.com>
2024-04-09 19:35:55 -0700

    fixed login

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,9 @@
 import { useEffect } from "react";
-import {useParams} from "react-router-dom"
 
 const Profile = ({user, fetchUser}) => {
-    const params = useParams()
 
     useEffect(() => {
-
-        fetchUser(params.id)
+        fetchUser()
     }, [])
 
     const userProfile = () => {
@@ -37,4 +34,44 @@ const Profile = ({user, fetchUser}) => {
     return (user ? userProfile() : checkForUser())
 }
 
-export default Profile
+export default Profile 
+
+// import {useParams} from "react-router-dom"
+
+// const Profile = ({user, fetchUser}) => {
+//     const params = useParams()
+
+//     useEffect(() => {
+
+//         fetchUser(params.id)
+//     }, [])
+
+//     const userProfile = () => {
+//         return (
+//             <div>
+//                 <h1>Welcome, {user.username}</h1>
+//             </div>
+//         )
+//     }
+
+//     const checkForUser = () => {
+//         let token = localStorage.getItem("authToken")
+
+//         if(!user && !token) {
+//             return (
+//                 <div>
+//                     <h1>403 Forbidden</h1>
+//                 </div>
+//             )
+//         }else if (!user) {
+//             return (
+//                 <div>
+//                     <h1>Loading ...</h1>
+//                 </div>
+//             )
+//         }
+//     }
+//     return (user ? userProfile() : checkForUser())
+// }
+
+// export default Profile
